bac934234fe43e2ec78dbdf2076ac754b69be62f
Sapizi <samokrutovandre261@gmail.com>
2025-08-28 09:54:41 +0500

    обновление меню 28,08,25

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <>
-      <img src="/VerticalMenu26_08.png" alt="" className="menu"/>
+      <img src="/VerticalMenu22_08.png" alt="" className="menu"/>
     </>
   );
 }
