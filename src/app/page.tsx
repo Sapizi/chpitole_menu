0f12231a1954573c6df3d6c49b1b01adec28b3ea
Sapizi <samokrutovandre261@gmail.com>
2025-10-15 09:39:18 +0500

    15.10

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <>
-      <img src="/VerticalMenu03_10.png" alt="menuuu" className="menu"/>
+      <img src="/VerticalMenu15_10.png" alt="menuuu" className="menu"/>
     </>
   );
 }
