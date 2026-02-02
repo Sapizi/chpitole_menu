0f0d4602c09c4e734fe5235b81736dce21fa760c
Sapizi <samokrutovandre261@gmail.com>
2026-02-02 23:09:48 +0500

    меню 2 февраля

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import "./globals.css";
 export default function Home() {
   return (
     <>
-      <img src="/VerticalMenu29_10.jpg" alt="menuuu" className="menu"/>
+      <img src="/VerticalMenu02_02.jpg" alt="menuuu" className="menu"/>
     </>
   );
 }
